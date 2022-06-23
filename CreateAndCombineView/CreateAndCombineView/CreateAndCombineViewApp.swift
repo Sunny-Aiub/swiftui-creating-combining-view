037668a6e4df84ab25c891b815c8ed57bd30d159
chowdhury_shaheb <mhcsunny@gmail.com>
2022-06-23 18:11:50 +0600

    project created

---
+++ CreateAndCombineView/CreateAndCombineView/CreateAndCombineViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAndCombineViewApp.swift
+//  CreateAndCombineView
+//
+//  Created by MACBOOK Pro - WSIT on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CreateAndCombineViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
